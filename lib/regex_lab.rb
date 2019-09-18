98bcf7b9468a11f585c3e715b42a579426916106
Mark Rego <rego121@gmail.com>
2019-09-18 16:35:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,21 +1,20 @@
-df
 
 def starts_with_a_vowel?(word)
-word.scan
+  word.match(/^[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan()
 end
 
 def words_five_letters_long(text)
-
+  text.scan()
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match()
 end
 
 def valid_phone_number?(phone)
-
+  phone.match()
 end
